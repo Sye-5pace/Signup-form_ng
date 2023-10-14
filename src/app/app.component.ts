9ecedd2ba01e2d3a2f22143fbd58b2ae9b51b6b9
Sye-5pace <owusilasyp00@gmail.com>
2023-10-14 11:56:21 +0100

    Form validation in-progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'signup-form_ng';
+  // @ViewChild('loginForm') loginForm: ElementRef;
+
+  email: string = '';
+  password: string = '';
+
+  onSubmit(loginForm: NgForm){
+    if(loginForm.form.valid){
+      console.log('Email Address:' + this.email)
+      console.log('Password:' + this.password)
+    }else{
+      console.log('Login failed')
+    }
+  }
 }
